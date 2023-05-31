11865154227e11687aac1ee9e8b6ac594f524b91
Adriano Almeida <adriano@z1.app>
2023-05-30 22:08:45 -0300

    first commit

---
+++ internal/entity/client_test.go
@@ -0,0 +1,43 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := NewClient("Joe doe", "j@j.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "Joe doe", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("Joe doe", "j@j.com")
+	err := client.Update("John doe", "j@j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "John doe", client.Name)
+	assert.Equal(t, "j@j.com", client.Email)
+}
+
+func TestUpdateClientWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("Joe doe", "j@j.com")
+	err := client.Update("", "j@j.com")
+	assert.Error(t, err, "name is required")
+}
+
+func TestAddAccountToClient(t *testing.T) {
+	client, _ := NewClient("Joe doe", "j@j")
+	account := NewAccount(client)
+	err := client.AddAccount(account)
+	assert.Nil(t, err)
+	assert.Equal(t, 1, len(client.Accounts))
+}
